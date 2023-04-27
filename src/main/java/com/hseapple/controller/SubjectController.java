b1f7362b0c35b0630ef1d129458cb7593d429e4e
Dayana Tasbauova <datasbauova@edu.hse.ru>
2023-04-27 15:43:02 +0300

    Added my project

---
+++ src/main/java/com/hseapple/controller/SubjectController.java
@@ -0,0 +1,4 @@
+package com.hseapple.controller;
+
+public class SubjectController {
+}
---
+++ src/main/java/com/hseapple/dao/CourseEntity.java
@@ -0,0 +1,35 @@
+package com.hseapple.dao;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "course", schema = "public")
+public class CourseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "subjectid")
+    private Integer subjectID;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    //getters and setters
+}
